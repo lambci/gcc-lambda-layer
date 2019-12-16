e9bec201b553a5beea7bf51b9a924e6de062be4a
Michael Hart <michael.hart.au@gmail.com>
2019-12-15 23:04:55 -0500

    Fix libc.so and libpthread.so issues

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Layers for AWS Lambda that allow your functions to use `gcc` and `c++` binaries.
 
 | gcc version | ARN |
 | --- | --- |
-| 7.3.1 | `arn:aws:lambda:${AWS::Region}:553035198032:layer:gcc-lambda2:1` |
+| 7.3.1 | `arn:aws:lambda:${AWS::Region}:553035198032:layer:gcc-lambda2:2` |
 
 ## Version ARNs for all other runtimes
 
 | gcc version | ARN |
 | --- | --- |
-| 7.2.1 | `arn:aws:lambda:${AWS::Region}:553035198032:layer:gcc72-lambda1:1` |
-| 4.8.5 | `arn:aws:lambda:${AWS::Region}:553035198032:layer:gcc48-lambda1:1` |
+| 7.2.1 | `arn:aws:lambda:${AWS::Region}:553035198032:layer:gcc72-lambda1:2` |
+| 4.8.5 | `arn:aws:lambda:${AWS::Region}:553035198032:layer:gcc48-lambda1:2` |
--- lambda2/build.sh
+++ lambda2/build.sh
@@ -3,4 +3,10 @@
 . ./config.sh
 
 docker run --rm -v "$PWD":/app lambci/yumda:2 sh -c \
-  "yum install -y ${YUM_PACKAGE} && cd /lambda/opt && zip -yr /app/layer.zip ."
+  "yum install -y ${YUM_PACKAGE} && \
+  sed -i 's_/opt/lib/libpthread.so.0_/usr/lib64/libpthread.so.0_' /lambda/opt/lib/libpthread.so && \
+  sed -i 's_/opt/lib/libc.so.6_/usr/lib64/libc.so.6_' /lambda/opt/lib/libc.so && \
+  sed -i 's_/opt/lib/ld-linux-x86-64.so.2_/usr/lib64/ld-linux-x86-64.so.2_' /lambda/opt/lib/libc.so && \
+  sed -i 's_/opt/lib/libm.so.6_/usr/lib64/libm.so.6_' /lambda/opt/lib/libm.so && \
+  sed -i 's_/opt/lib/libmvec.so.1_/usr/lib64/libmvec.so.1_' /lambda/opt/lib/libm.so && \
+  cd /lambda/opt && zip -yr /app/layer.zip ."
